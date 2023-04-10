c428ea2187d5e4a0229b9b1d1a795146c524d28f
Dipronil1998 <dipronildas.net@gmail.com>
2023-04-11 00:55:07 +0530

    issue fixed

--- src/Component/Menu.jsx
+++ src/Component/Menu.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthService from "../Service/AuthService";
 
 const Menu = () => {
     // const isLogin = localStorage.getItem("user");
+    const [currentUser, setCurrentUser] = useState(null)
 
     const logOut = () => {
         AuthService.logout();
+        setCurrentUser(null)
     }
 
+    useEffect(() => {
+        const currentUser = localStorage.getItem("user");
+        console.log(currentUser, "isLogin");
+        setCurrentUser(currentUser)
+    })
+
     return (
-        <>
-            <Link to="/">Login</Link>
-            <Link to="/signup">Signup</Link>
-            <Link to="/"  onClick={logOut}>Logout</Link>
+        <> 
+            {
+                currentUser 
+                ? <Link to="/"  onClick={logOut}>Logout</Link>
+                : (
+                    <>
+                        <Link to="/">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </>
+                )
+            }
+            
         </>
         
     )
